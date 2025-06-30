7deb46b76373571f22fa6a1f658414d24d6bb062
estudanteVictorH <yaros.brito@escola.pr.gov.br>
2025-06-30 10:40:29 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,37 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+
+criaCartao(
+    'Animes' ,
+    'Qual anime foi considerado o melhor do ano de 2025' , 
+    'Solo Leveling',
+  )
+
+
+criaCartao(
+    'Jogos' , 
+    'Em que ano o valorant foi lançado?' , 
+    'Foi lançado em 2020'
+)
